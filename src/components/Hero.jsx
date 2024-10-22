9bdb65442f38e60dc0257dba5e4a3f50dc913643
smritizz <smritizz1510@gmail.com>
2024-10-22 12:22:26 +0530

    chnaged build settings

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div>
+        <div className="mb-[180px]">
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Smriti</span>
           </h1>
@@ -23,7 +23,6 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
       <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
