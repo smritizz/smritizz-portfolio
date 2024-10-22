324b526222aaa1b53334c50b8f77199e3236d0e9
smritizz <smritizz1510@gmail.com>
2024-10-22 14:54:06 +0530

    created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,25 +5,29 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className={`relative w-full mx-auto h-screen`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[100px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 violet-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Smriti</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} text-white-100`}>
             I am a fullStack developer. Transforming ideas into products is my calling.<br className='sm:block hidden' />
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+
+      <ComputersCanvas/>
+      {/* <p className={`${styles.heroSubText} mt-2  text-white-100`}>
+            I am a fullStack developer. Transforming ideas into products is my calling.<br className='sm:block hidden' />
+          </p> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -75,10 +75,9 @@ const Works = () => {
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
-      {/* <div className='w-full flex'>
+      <div className='w-full flex'>
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          className='mt-3 text-secondary text-[17px] leading-[30px] w-full'
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
@@ -86,9 +85,9 @@ const Works = () => {
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
-      </div> */}
+      </div>
 
-      <div className='mt-20 flex flex-wrap gap-5'>
+      <div className='mt-16 flex flex-wrap gap-5'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
