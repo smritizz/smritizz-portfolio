72d6ef6a99845d0ad2419df431a33639b0a21d32
smritizz <smritizz1510@gmail.com>
2024-10-22 12:56:00 +0530

    chnaged config setting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false, // Disable minification for debugging purposes
+    sourcemap: true, // Generate source maps for easier debugging
+    outDir: 'dist', // Output directory
+  },
 })
