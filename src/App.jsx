4230e66903277a0bff51175d95f41648d07af09d
smritizz <smritizz1510@gmail.com>
2024-10-22 13:23:22 +0530

    chnaged build settings

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ const App = () => {
           <Hero />
         </div>
         <About />
+        <Works />
+
         <Experience />
         <Tech />
-        <Works />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
