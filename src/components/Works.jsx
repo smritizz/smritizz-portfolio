5caa5db0d012794d868974ac30077ab535e93e1b
smritizz <smritizz1510@gmail.com>
2024-10-22 13:53:57 +0530

    testing

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -73,9 +73,9 @@ const Works = () => {
       {/* <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
+      </motion.div> */}
 
-      <div className='w-full flex'>
+      {/* <div className='w-full flex'>
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
@@ -86,15 +86,13 @@ const Works = () => {
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
-      </div>
+      </div> */}
 
       <div className='mt-20 flex flex-wrap gap-7'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
-    </> */}
-    hello 
     </>
   );
 };
